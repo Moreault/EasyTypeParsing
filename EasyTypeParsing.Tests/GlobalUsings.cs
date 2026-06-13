ab6ce9511ddf1b9093de7ca4b631fbd38becdd4a
Mathieu Moreault <seranx@gmail.com>
2026-06-13 06:41:15 -0400

    Updated to .NET 10

--- EasyTypeParsing.Tests/GlobalUsings.cs
+++ EasyTypeParsing.Tests/GlobalUsings.cs
@@ -1,13 +1,14 @@
 // Global using directives
 
 global using EasyTypeParsing.Tests.Utilities;
-global using FluentAssertions;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
 global using System;
 global using System.Collections.Generic;
 global using System.Globalization;
 global using System.Net;
 global using System.Numerics;
+global using ToolBX.AssertBox;
+global using ToolBX.AssertBox.Assertions;
 global using ToolBX.Dummies;
 global using ToolBX.EasyTypeParsing;
 global using ToolBX.EasyTypeParsing.Resources;
